a26dbf71caf9217eeeb9641e8e2babe3d650466e
ThiMy46 <daothimy46@gmail.com>
2018-05-12 18:05:50 +0700

    Thêm giao diện Chat(message_list, item_message_received, item_message_send)

--- app/src/main/java/com/appchat/MessageList.java
+++ app/src/main/java/com/appchat/MessageList.java
@@ -3,11 +3,11 @@ package com.appchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MessageList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.item_message_received);
+        setContentView(R.layout.activity_message_list);
     }
 }
