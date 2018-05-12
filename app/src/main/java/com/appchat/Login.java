4a6879259efe4828264e4881e435d6d56885490a
ThiMy46 <daothimy46@gmail.com>
2018-05-12 15:37:34 +0700

    Thêm giao diện Login và Singup

--- app/src/main/java/com/appchat/Login.java
+++ app/src/main/java/com/appchat/Login.java
@@ -3,11 +3,11 @@ package com.appchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sing_up);
+        setContentView(R.layout.activity_login);
     }
 }
